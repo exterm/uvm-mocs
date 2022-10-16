af6db17ca26f11c08fe941b2bfd47ff0e3013c85
Philip Müller <mail@philip.in-aachen.net>
2022-10-16 17:30:21 -0400

    extract rendering of videos and plots to separate file

---
+++ assignment-02/plots_from_simulation.py
@@ -0,0 +1,65 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as ani
+import matplotlib.colors as clrs
+import numpy as np
+
+def plots(sim, render_interval, video_filename=None, image_filename=None):
+  def animate(i):
+      worldplot.set_data(sim.history[i])
+      return [worldplot]
+
+  config = sim.config
+
+  print("Initializing plot..")
+  fig = plt.figure()
+  ax = fig.add_subplot()
+  ax.axis(False)
+
+  colors = clrs.ListedColormap(['white', 'green', 'red'])  # type: ignore
+  worldplot = ax.imshow(np.zeros((config.height, config.width)),
+                        cmap=colors,
+                        aspect='equal',
+                        interpolation='none',
+                        vmin=0,
+                        vmax=2)
+
+  print("Animation starting...")
+  anim = ani.FuncAnimation(fig,
+                          animate,
+                          len(sim.history),
+                           interval=render_interval,
+                          blit=True,
+                          repeat=False)
+  plt.tight_layout(pad=0)
+
+  if video_filename:
+    print("Writing video...")
+    writervideo = ani.FFMpegWriter(fps=1000 // render_interval)
+    print("Writing video...")
+    anim.save('output/video_filename', writer=writervideo)
+
+  if image_filename:
+    print("Writing image...")
+    plt.savefig('output/animated_2d_ca.png', dpi=600)
+
+  num_cells = config.width * config.height
+  fig, ax1 = plt.subplots()
+  burn_history = [s[0] / num_cells for s in sim.stats]
+  burnline, = ax1.plot(
+      burn_history, label="Fraction of cells burning", color="red")
+  ax1.set_ylabel("Fraction of cells burning")
+  ax1.set_ylim(ymin=0)
+  max_fire = np.max(burn_history) / num_cells
+  max_fire_line = ax1.axhline(y=max_fire,  color='k', linestyle='dotted', lw=2,
+                              label=f'Largest fire: ${round(max_fire * 100, 2)}\\%$')
+
+  ax2 = ax1.twinx()
+  # ax2.tick_params(axis='y', labelcolor='blue')
+  treeline, = ax2.plot([s[1] / num_cells
+                      for s in sim.stats], label="Tree density")
+  ax2.set_ylabel("Fraction of cells with trees")
+  plt.legend(handles=[burnline, treeline, max_fire_line])
+  plt.xlabel("Step")
+  plt.title("Forest fire simulation")
+  print("Show!")
+  plt.show()
--- assignment-02/run-simple-forest-fire.py
+++ assignment-02/run-simple-forest-fire.py
@@ -1,8 +1,5 @@
 from core_simulation import (forestSimulation, ForestConfig)
-import matplotlib.pyplot as plt
-import matplotlib.animation as ani
-import matplotlib.colors as clrs
-import numpy as np
+from plots_from_simulation import (plots)
 import time
 import cProfile
 
@@ -29,7 +26,7 @@ import cProfile
 
 WIDTH = 120
 HEIGHT = 100
-STEPS = 250
+STEPS = 25
 RENDER_INTERVAL = 25
 P_TREE = 0.01 # probability of a cell initially containing a tree
 P_SPROUT = 0.0005 # likelihood of an empty cell sprouting a tree each step
@@ -49,54 +46,4 @@ executionTime = (time.time() - startTime)
 print('Simulation Execution time in seconds: ' + str(executionTime))
 sim.print_stats()
 
-def animate(i):
-    worldplot.set_data(sim.history[i])
-    return [worldplot]
-
-print("Initializing plot..")
-fig = plt.figure()
-ax = fig.add_subplot()
-ax.axis(False)
-
-colors = clrs.ListedColormap(['white', 'green', 'red'])  # type: ignore
-worldplot = ax.imshow(np.zeros((config.height, config.width)),
-                      cmap=colors,
-                      aspect='equal',
-                      interpolation='none',
-                      vmin=0,
-                      vmax=2)
-
-print("Animation starting...")
-anim = ani.FuncAnimation(fig,
-                         animate,
-                         STEPS,
-                         interval=RENDER_INTERVAL,
-                         blit=True,
-                         repeat=False)
-plt.tight_layout(pad=0)
-
-# writervideo = ani.FFMpegWriter(fps=1000 // RENDER_INTERVAL)
-# print("Writing video...")
-# anim.save('output/forest_fire.mp4', writer=writervideo)
-
-# print("Writing image...")
-# plt.savefig('output/animated_2d_ca.png', dpi=600)
-
-fig, ax1 = plt.subplots()
-burn_history = [s[0] / (HEIGHT * WIDTH) for s in sim.stats]
-burnline, = ax1.plot(burn_history, label="Fraction of cells burning", color="red")
-ax1.set_ylabel("Fraction of cells burning")
-# ax1.set_ylim(ymin=0)
-max_fire = np.max(burn_history) / (HEIGHT * WIDTH)
-max_fire_line = ax1.axhline(y=max_fire,  color='k', linestyle='dotted', lw=2,
-            label=f'Largest fire: ${round(max_fire * 100, 2)}\\%$')
-
-ax2 = ax1.twinx()
-# ax2.tick_params(axis='y', labelcolor='blue')
-treeline, = ax2.plot([s[1]/ (HEIGHT * WIDTH) for s in sim.stats], label="Tree density")
-ax2.set_ylabel("Fraction of cells with trees")
-plt.legend(handles=[burnline, treeline, max_fire_line])
-plt.xlabel("Step")
-plt.title("Forest fire simulation")
-print("Show!")
-plt.show()
+plots(sim, RENDER_INTERVAL)
