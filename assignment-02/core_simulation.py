4bb10df23c3f096b4935bafb8cf90463606a761e
Philip Müller <mail@philip.in-aachen.net>
2022-10-17 14:59:21 -0400

    add low-effort tests for simulation class

--- assignment-02/core_simulation.py
+++ assignment-02/core_simulation.py
@@ -1,3 +1,4 @@
+import unittest
 import numpy as np
 from collections import namedtuple
 
@@ -154,3 +155,57 @@ class forestSimulation:
         print("Statistics:")
         print(f" Largest fire: {largest_fire}")
         print(f" Highest tree density: {highest_tree_density}")
+
+# Unit tests
+class TestSimulation(unittest.TestCase):
+
+    # these are low-effort tests and should not be used as an example for what good tests look like.
+    # Better tests would be more thorough and would test the individual methods of the simulation class.
+    # if we're testing small, fast things we can also use property-based testing (e.g. Hypothesis)
+    # for much more concise tests.
+
+    def test_basic_properties(self):
+        config = forestConfig(
+            height=10,
+            width=10,
+            p_tree=0.5,
+            p_lightning=0.1,
+            p_sprout=0.1,
+            p_propagate=0.1,
+            seed=0
+        )
+        sim = forestSimulation(config)
+        sim.simulate(steps=10)
+
+        self.assertEqual(len(sim.history), 10)
+        self.assertEqual(len(sim.stats), 10)
+
+        np.testing.assert_array_equal(sim.history[-1], sim.currentState)
+
+    def test_golden_samples(self):
+        config = forestConfig(
+            height=10,
+            width=10,
+            p_tree=0.5,
+            p_lightning=0.1,
+            p_sprout=0.1,
+            p_propagate=0.1,
+            seed=0
+        )
+        sim = forestSimulation(config)
+        sim.simulate(steps=10)
+        # golden sample (brittle, but will do for now)
+        expected = np.array([[1., 0., 1., 0., 1., 0., 1., 0., 2., 1.,],
+                             [1., 1., 2., 0., 1., 1., 0., 2., 2., 2.,],
+                             [0., 1., 1., 2., 1., 2., 1., 0., 2., 1.,],
+                             [2., 0., 1., 0., 0., 0., 2., 0., 2., 1.,],
+                             [0., 0., 1., 0., 2., 2., 2., 2., 2., 1.,],
+                             [0., 1., 1., 0., 2., 1., 1., 1., 1., 2.,],
+                             [0., 2., 0., 0., 1., 1., 0., 0., 0., 2.,],
+                             [2., 2., 0., 0., 2., 1., 1., 0., 1., 2.,],
+                             [1., 0., 0., 0., 2., 2., 2., 1., 1., 1.,],
+                             [1., 2., 2., 0., 0., 0., 0., 2., 2., 1.,],])
+        np.testing.assert_array_equal(sim.currentState, expected)
+
+if __name__ == '__main__':
+    unittest.main()
